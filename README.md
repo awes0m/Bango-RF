e30d223a3a0882d8742fe426e776eab89d082783
awes0m <somsubhra.pandit@gmail.com>
2022-04-29 15:13:52 +0530

    Added custom user profile manager and migrations

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ Also includes Profile, and User Management.
 
 To create a basic Vagrant file of ubuntu image - `vagrant init ubuntu/bionic64` (optional)
 
+
+## Initial Setup
 create a new project with `django-admin startproject {project-name}`
 create a new app with `python manage.py startapp {app-name}`
 
@@ -16,3 +18,13 @@ add the {app-name} to the settings.py file in INSTALLED_APPS- this registers the
 Since we are using the django-rest-framework , we should also add the `'rest_framework'` and `'rest_framework.authtoken'` package name in settings.py>INSTALLED_APPS to register the framework with the project
 
 to run the server `python manage.py runserver`
+
+
+## Create a Model
+After defining custom user models, we need to register the user model with the framework.
+in the settings.py file, add the following line at teh end:
+AUTH_USER_MODEL = '{app-name}.UserProfileManager'
+
+## Migrating the Models to the Database
+`python manage.py makemigrations {app-name}` - this creates a migration file for the app
+`python manage.py migrate` - this migrates the app to the database. ie- creates the tables needed for the project
--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,82 @@
 from django.db import models
 
-# Create your models here.
+# Inbuilt user model from Django
+from django.contrib.auth.models import AbstractBaseUser
+
+# PermissionsMixin is a mixin class that provides
+# a set of methods and fields required by the PermissionRequiredMixin to
+# handle user persmissions
+from django.contrib.auth.models import PermissionsMixin
+
+# BaseUserManager is a base class for user model managers
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserProfileManager(BaseUserManager):
+    """Manager for user profiles"""
+
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError("User must have an email address")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        # [set password] encrypts the password before saving it to the database
+        user.set_password(password)
+        user.save(using=self._db)  # saves to db
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database models for users in the system.
+    :param AbstractBaseUser: Inbuilt user model from Django
+    :PermissionsMixin: PermissionsMixin is a mixin class that provides
+    a set of methods and fields require
+    by the PermissionRequiredMixin to handle user persmissions.
+    :return: name, email, password, is_active, is_staff, is_superuser
+    """
+
+    email = models.EmailField(max_length=255, unique=True)
+    """gets the  user email"""
+    # unique-is used to make sure that no two users can have the same email
+
+    name = models.CharField(max_length=255)
+    """ gets the user's name"""
+
+    is_active = models.BooleanField(default=True)
+    """ gets the active status,
+    default true, but can be deactivated in future"""
+
+    is_staff = models.BooleanField(default=False)
+    """gets the active status,
+    default true, but can be deactivated in future"""
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = "email"  # takes the email as the username
+    REQUIRED_FIELDS = ["name"]  # name is required
+
+    def get_full_name(self):
+        """Retrieve the users full name"""
+        return self.name
+
+    def get_short_name(self):
+        """Retrieve the users short name"""
+        return self.name
+
+    def __str__(self):
+        """Return string representation of our user"""
+        return self.email
