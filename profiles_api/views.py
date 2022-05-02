099460866c23a9f04cdcff11ee4cfd0eba5ea883
awes0m <somsubhra.pandit@gmail.com>
2022-05-02 17:08:51 +0530

    added login and auth token

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import viewsets, status, filters
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from profiles_api import serializers, models, permissions
 
@@ -100,9 +102,19 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
-    permission_classes = (permissions.UpdateOwnProfile,)
-    filter_backends = (filters.SearchFilter,)
+    permission_classes = (
+        permissions.UpdateOwnProfile,
+    )  # only allow authenticated users
+    filter_backends = (filters.SearchFilter,)  # defines the serach parameters
+    # ie- name, email
     search_fields = (
         "name",
         "email",
     )
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+
+    # override the default ObtainAuthToken class for the API
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
