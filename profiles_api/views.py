252228f82edd531dba3c4e64bb3165d63efb2a96
awes0m <somsubhra.pandit@gmail.com>
2022-05-02 16:49:08 +0530

    added search to api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import viewsets, status  # contains the status codes
+from rest_framework import viewsets, status, filters
 from rest_framework.authentication import TokenAuthentication
 
 from profiles_api import serializers, models, permissions
@@ -101,3 +101,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = (
+        "name",
+        "email",
+    )
